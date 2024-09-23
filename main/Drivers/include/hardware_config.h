172f32d3dba50d9009f7cec98d3e05f6bae2937c
hovey <hovey@ubuntu24.04>
2024-09-23 17:00:24 +0800

    set my board conf

--- main/Drivers/include/hardware_config.h
+++ main/Drivers/include/hardware_config.h
@@ -1,10 +1,9 @@
 #ifndef HARDWARE_CONFIG_H
 #define HARDWARE_CONFIG_H
 
+#define MY_BOARD_CONF 1 /*0:demo 1:my PCB*/
 /*LCD CONFIG*/
-#define SPI_SDA 1
-#define SPI_SCL 2
-#define SPI_CS  42
+
 #define SCREEN_TYPE 2 /*1:  4 inch               2:  2.1 inch*/
 /*size*/
 #define LCD_H_RES              480
@@ -14,6 +13,10 @@
 /*rgb_clock*/
 #define LCD_PIXEL_CLOCK_HZ     (18 * 1000 * 1000)
 /*RGB_PINS*/
+#if MY_BOARD_CONF == 0
+#define SPI_SDA 1
+#define SPI_SCL 2
+#define SPI_CS  42
 #define PIN_NUM_HSYNC          38
 #define PIN_NUM_VSYNC          39
 #define PIN_NUM_DE             40
@@ -35,7 +38,34 @@
 #define PIN_NUM_DATA14         18 // R3
 #define PIN_NUM_DATA15         17 // R4
 #define PIN_NUM_DISP_EN        -1
+#endif
 
+#if MY_BOARD_CONF == 1
+#define SPI_SDA 1
+#define SPI_SCL 2
+#define SPI_CS  45
+#define PIN_NUM_HSYNC          40
+#define PIN_NUM_VSYNC          39
+#define PIN_NUM_DE             38
+#define PIN_NUM_PCLK           15
+#define PIN_NUM_DATA0          41 // B1
+#define PIN_NUM_DATA1          42 // B2
+#define PIN_NUM_DATA2          48 // B3
+#define PIN_NUM_DATA3          47 // B4
+#define PIN_NUM_DATA4          21 // B5
+#define PIN_NUM_DATA5          14 // G0
+#define PIN_NUM_DATA6          13 // G1
+#define PIN_NUM_DATA7          12 // G2
+#define PIN_NUM_DATA8          11 // G3
+#define PIN_NUM_DATA9          10 // G4
+#define PIN_NUM_DATA10         9  // G5
+#define PIN_NUM_DATA11         3  // R1
+#define PIN_NUM_DATA12         15 // R2
+#define PIN_NUM_DATA13         16 // R3
+#define PIN_NUM_DATA14         17 // R4
+#define PIN_NUM_DATA15         18 // R5
+#define PIN_NUM_DISP_EN        -1
+#endif
 
 #endif
 
